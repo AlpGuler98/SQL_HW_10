2ac9e3c6d19ddd4e042823a79baf72510748b8a7
AlpGuler98 <84928227+AlpGuler98@users.noreply.github.com>
2021-07-22 13:57:13 +0300

    Create hw_10.sql

---
+++ hw_10.sql
@@ -0,0 +1,11 @@
+-- Query 1
+SELECT city.city, country.country FROM city
+LEFT JOIN country ON city.country_id = country.country_id;
+
+-- Query 2
+SELECT payment.payment_id, customer.first_name, customer.last_name FROM customer
+RIGHT JOIN payment ON customer.customer_id = payment.customer_id;
+
+-- Query 3
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM rental
+FULL JOIN customer ON customer.customer_id = rental.customer_id;
